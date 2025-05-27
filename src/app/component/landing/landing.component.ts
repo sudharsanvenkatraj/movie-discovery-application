d9a7f697c63c79073dad9b8d237b46e7bcf7e9ad
sudharsan <writetosudharsn@gmail.com>
2025-05-27 13:55:01 +0530

    fix: updated loader and code optimization

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -1,54 +1,35 @@
 import { Component } from '@angular/core';
 import { TmdbServiceTsService } from '../../core/services/tmdb.service.ts.service';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-landing',
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
+
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
-  // standalone: true,
 })
 export class LandingComponent {
   constructor(private tmdbServiceTsService: TmdbServiceTsService) {
   }
-  feelGoodList: any = [];
-  actionList: any;
-  mindBendersList: any;
-  isFeelGood: boolean = false;
-  isActionFix: boolean = false;
-  isMindBenders: boolean = false;
-
-
+  movieList: any = [];
+  loading: boolean = false;
   ngOnInit(): void {
   }
 
 
-  getFeelGood() {
-    this.tmdbServiceTsService.getMovieDetails(10751).subscribe((iye) => {
-      this.feelGoodList = iye.results;
-      this.isFeelGood = true;
-      this.isActionFix = false;
-      this.isMindBenders= false;
-      console.log(this.feelGoodList)
-    });
-
-  }
-  getActionFix() {
-    this.tmdbServiceTsService.getMovieDetails(28).subscribe((iye) => {
-      this.actionList = iye.results
-      this.isActionFix = true;
-      this.isFeelGood = false;
-      this.isMindBenders= false;
-
-    });
-  }
-  getMindBenders() {
-    this.tmdbServiceTsService.getMovieDetails(878).subscribe((iye) => {
-      this.mindBendersList = iye.results
-      this.isActionFix = false;
-      this.isFeelGood = false;
-      this.isMindBenders= true;
+  getMovies(genres_id: number) {
+    this.loading = true;
+    this.tmdbServiceTsService.getMovieDetails(genres_id).subscribe({
+      next: (res) => {
+        setTimeout(() => {
+          this.movieList = res.results;
+          this.loading = false;
+        }, 1000)
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
     });
   }
 }
