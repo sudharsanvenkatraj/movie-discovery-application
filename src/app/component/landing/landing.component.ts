38179f967054f3159be452a6f776b54d328e7070
naveena <naveetest26@gmail.com>
2025-05-27 12:30:52 +0530

    ui updated

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -15,23 +15,40 @@ export class LandingComponent {
   feelGoodList: any = [];
   actionList: any;
   mindBendersList: any;
+  isFeelGood: boolean = false;
+  isActionFix: boolean = false;
+  isMindBenders: boolean = false;
 
 
   ngOnInit(): void {
-    this.getFeelGood();
   }
 
 
   getFeelGood() {
     this.tmdbServiceTsService.getMovieDetails(10751).subscribe((iye) => {
-      this.feelGoodList = iye;
+      this.feelGoodList = iye.results;
+      this.isFeelGood = true;
+      this.isActionFix = false;
+      this.isMindBenders= false;
+      console.log(this.feelGoodList)
     });
 
   }
-  getAction() {
-    this.tmdbServiceTsService.getMovieDetails(28).subscribe((iye) => this.actionList = iye);
+  getActionFix() {
+    this.tmdbServiceTsService.getMovieDetails(28).subscribe((iye) => {
+      this.actionList = iye.results
+      this.isActionFix = true;
+      this.isFeelGood = false;
+      this.isMindBenders= false;
+
+    });
   }
   getMindBenders() {
-    this.tmdbServiceTsService.getMovieDetails(878).subscribe((iye) => this.mindBendersList = iye);
+    this.tmdbServiceTsService.getMovieDetails(878).subscribe((iye) => {
+      this.mindBendersList = iye.results
+      this.isActionFix = false;
+      this.isFeelGood = false;
+      this.isMindBenders= true;
+    });
   }
 }
